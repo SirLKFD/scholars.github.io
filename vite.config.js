779cac32a1c05237fd5893bf3af491c2adf12d70
Lord Kent F. Dinampo <lordkentdinampo@gmail.com>
2024-12-18 21:46:04 +0800

    Test

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    open: true, // Automatically open the browser
+    port: 3000, // Optional: Set the port number (default is 5173)
+  },
 })
