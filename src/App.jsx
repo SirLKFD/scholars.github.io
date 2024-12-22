4a97cff0c1945a14b7ddd1ca60cd305defd42e85
Lord Kent F. Dinampo <lordkentdinampo@gmail.com>
2024-12-23 00:18:08 +0800

    Tutorial Files

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,29 @@
 import Header from './components/header.jsx';
 import Footer from './components/footer.jsx';
 import Card from './components/cards.jsx';
-import Button from './components/button.jsx';
+import ButtonTutorialEvent from './components/buttonTutorialEvent.jsx';
+import ProfilePicture from './components/ProfilePicture.jsx';
+import MyComponent from './components/myComponent.jsx';
+import Counter from './components/Counter.jsx';
 
 //import Background from "../src/components/background.jsx";
 
 
 function App() {
-  return (
-    <div>
 
-    <h1>Hello</h1>
-      <Button />
+    return (
+    <div>
+        <Header/>
+        <Footer/>
+        <Card/>
+        <ButtonTutorialEvent/>
+        <ProfilePicture/>
+        <MyComponent/>
+<Counter/>
+
       </div>
-  
+
+
   );
 }
 
